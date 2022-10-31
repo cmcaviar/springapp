5dc84620396e2baf92e7e3f55b657cc3376bbc77
cmcaviar <kproo626@gmail.com>
2022-10-31 13:15:44 +0300

    Теперь кириллица отображается

--- src/main/java/cmcaviar/config/DispatcherServletInitializer.java
+++ src/main/java/cmcaviar/config/DispatcherServletInitializer.java
@@ -1,8 +1,9 @@
 package cmcaviar.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
@@ -22,6 +23,10 @@ public class DispatcherServletInitializer extends AbstractAnnotationConfigDispat
     @Override
     public void onStartup(ServletContext aServletContext) throws ServletException {
         super.onStartup(aServletContext);
+        FilterRegistration.Dynamic encodingFilter = aServletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
         registerHiddenFieldFilter(aServletContext);
     }
 
@@ -29,4 +34,5 @@ public class DispatcherServletInitializer extends AbstractAnnotationConfigDispat
         aContext.addFilter("hiddenHttpMethodFilter",
                 new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
+
 }
